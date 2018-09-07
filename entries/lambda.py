410f541fcd6a775be469a6378f5ebf66f57964fe
Craig Davis <cdavis@stoneydavis.com>
2018-09-06 23:43:59 -0500

    Updated to workout the kinks. now using boto3 directly for dynamodb. removed most of the dependancy on aws_lambda

--- entries/lambda.py
+++ entries/lambda.py
@@ -1,8 +1,14 @@
+import decimal
 import boto3
-import aws_lambda.dynamodb as dynamodb
+import boto3.dynamodb.types
 import json
 import logging
 import os
+import pickle
+import json
+from json import JSONEncoder
+import datetime
+
 FORMAT = '%(asctime)-15s %(levelname)s %(module)s.%(funcName)s %(message)s'
 DATEFMT = "%Y-%m-%d %H:%M:%S"
 logging.basicConfig(level=logging.INFO, format=FORMAT, datefmt=DATEFMT)
@@ -10,30 +16,60 @@ LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
 deserializer = boto3.dynamodb.types.TypeDeserializer()
 
+RESPONSE_DEFAULT_HEADERS = {"Content-Type": "application/json"}
 
-def display_selected_entries(ddb, event):
-    results, count = ddb.scan(os.environ['DDBTable'])
-    results = [{k: deserializer.deserialize(v) for k,v in item.items()} for item in results['Items']]
-    return aws_lambda.generate_return(200, results)
+class PythonObjectEncoder(JSONEncoder):
 
-def display_all_entries(ddb, event):
+    def default(self, obj):
+        if isinstance(obj, (datetime.datetime, datetime.date)):
+            return obj.isoformat()
+        elif isinstance(obj, (decimal.Decimal,)):
+            return float(obj)
+        elif isinstance(
+            obj, (list, dict, str, int, float, bool, type, type(None), bytes)
+        ):
+            return JSONEncoder.default(self, obj)
+        else:
+            logging.warning(
+                "Unable to find matching encoder for type %s. Using pickle",
+                str(type(obj)),
+            )
+        return {"_python_object": pickle.dumps(obj)}
+
+def generate_response(status, data, headers=None):
+    if not headers:
+        headers = RESPONSE_DEFAULT_HEADERS
+    return {
+        "statusCode": status,
+        "headers": headers,
+        "body": json.dumps(data, cls=PythonObjectEncoder, sort_keys=True),
+    }
+
+def display_selected_entries(table, event):
     try:
         int(event['pathParameters']['id_number'])
     except ValueError:
-        return aws_lambda.generate_return(400, {'error': 'Must provide integer for id_number'})
-    results = ddb.get(os.environ['DDBTable'], int(event['pathParameters']['id_number']))
-    return aws_lambda.generate_return(200, results)
+        return generate_response(400, {'error': 'Must provide integer for id_number'})
+    results = table.get_item(Key={'id': int(event['pathParameters']['id_number'])})
+    return generate_response(200, results['Item'])
+
+def display_all_entries(table, event):
+    results = table.scan()
+    LOGGER.info(results['Items'])
+    return generate_response(200, [int(r['id']) for r in results['Items']])
 
 def main(event, context):
-    resp = aws_lambda.generate_return(
+    resp = generate_response(
         400, {'error': 'Invalid resource', 'resource': event['resource']})
     func_calls = {
         '/id/{id_number}': {'func': display_selected_entries},
         '/id': {'func': display_all_entries},
     }
+    LOGGER.info('Hanlding request for %s', event['resource'])
     if event['resource'] in list(func_calls.keys()):
-        ddb = dynamodb.DynamoDB()
-        resp = func_calls[event['resource']]['func'](ddb, event)
+        ddb = boto3.resource('dynamodb')
+        table = ddb.Table(os.environ['DDBTable'])
+        resp = func_calls[event['resource']]['func'](table, event)
 
     LOGGER.debug(resp)
     return resp
